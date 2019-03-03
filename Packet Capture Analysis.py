f7da9553b237395e37fc366b2f10d194b7656c8b
evil-scientist <suryansh@evilscientist.cc>
2019-03-03 23:34:26 +0100

    Update Python file

--- Packet Capture Analysis.py
+++ Packet Capture Analysis.py
@@ -8,67 +8,98 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 from matplotlib.patches import Rectangle
-data = pd.read_csv("data/netflix_results.csv") 
+data = pd.read_csv("Data/Cafeteria_2_results.csv") 
+# Preview the first 5 lines of the loaded data 
+data.head()
 
 
 # In[2]:
 
 
-# Preview the first 5 lines of the loaded data 
-data.head()
-
-
-# In[3]:
-
-
-#Number of Packets in captured Session
-data['frame.number'].count()
-
-
-# In[4]:
-
-
+print('The number of frames that were captured: ', data['frame.number'].count(), ' frames')
 print('The largest frame that was captured: ', data['frame.len'].max(), ' bytes')
 print('The smallest frame that was captured: ', data['frame.len'].min(), ' bytes')
 print('The lowest radio signal strength: ', data['wlan_radio.signal_dbm'].max(),' dBm')
 print('The highest radio signal strength: ', data['wlan_radio.signal_dbm'].min(),' dBm')
 
 
-# In[5]:
+# In[3]:
 
 
-# How many Packets are there for each Frame Type?
-data['wlan.fc.type'].value_counts()
+direction = []
+for x in data.groupby('wlan.fc.ds')['frame.number'].nunique():
+    direction.append(x/data['frame.number'].count()*100)
+if len(direction) != 4:
+    direction.append(0)
+#print(direction)
 
 
 # In[6]:
 
 
 #Plot the distribution:
-ftypes = ['Management Frame','Control Frame','Data Frame']
+ftypes = ['STA to STA','STA to AP','AP to STA','AP to AP']
 pos = np.arange(len(ftypes))
-data.groupby('wlan.fc.type')['frame.number'].nunique().plot(kind='bar')
-plt.title("Distribution of frames on type")
-plt.xlabel("Frame type")
-plt.ylabel("Number of Packets")
+#data.groupby('wlan.fc.ds')['frame.number'].nunique().plot(kind='bar')
+rect1 = plt.bar(x=pos, height=direction, width=0.8, align='center', color=['#1F77B4','#FF7F0E','#2CA02C','#D62728'])
+plt.title("Distribution of Traffic Direction")
+plt.xlabel("Traffic Direction")
+plt.ylabel("Packets %")
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width()/2., 1.0*height,
+                '%0.2f%%' % (height),
+                ha='center', va='bottom')
+autolabel(rect1)
 plt.xticks(pos,ftypes,rotation=0)
 plt.show()
 
 
-# In[7]:
+# In[ ]:
+
+
+# How many Packets are there for each Frame Type?
+data['wlan.fc.type'].value_counts()
+
+
+# In[ ]:
+
+
+types = list(data['wlan.fc.type'].value_counts())
+types = (types/data['frame.number'].count())*100
+print(types)
+
+
+# In[ ]:
+
+
+
+#Plot the distribution:
+ftypes = ['Management Frame','Control Frame','Data Frame','Corrupted Frame']
+pos = np.arange(len(ftypes))
+#data.groupby('wlan.fc.type')['frame.number'].nunique().plot(kind='bar')
+rect2 = plt.bar(x=pos, height=types, width=0.5, align='center', color=['#1F77B4','#FF7F0E','#2CA02C','#D62728'])
+plt.title("Distribution of frames based on type")
+plt.xlabel("Frame type")
+plt.ylabel("Packets %")
+autolabel(rect2)
+plt.xticks(pos,ftypes,rotation=0)
+plt.show()
+
+
+# In[ ]:
 
 
 # How many Packets are there for each SubFrame Type?
-data.groupby('wlan.fc.type')['wlan.fc.subtype'].value_counts()
+for x in data.groupby('wlan.fc.type')['wlan.fc.subtype'].value_counts():
+    print (x)
 
 
-# In[8]:
-
-
-data.groupby(['wlan.fc.type','wlan.fc.subtype'])['frame.number'].nunique()
-
-
-# In[20]:
+# In[ ]:
 
 
 #Plot the distribution:
@@ -81,66 +112,62 @@ plt.title("Distribution of frames on subtype")
 plt.xlabel("Frame Subtype")
 plt.ylabel("Number of Packets")
 #plt.xticks(pos,ftypes,rotation=0)
+plt.xticks(rotation=0)
+
 plt.show()
 
 
-# In[10]:
+# In[ ]:
 
 
 #Which direction does the data flow (number of Packets)?
 data['wlan.fc.ds'].value_counts()
 
 
-# In[11]:
-
-
-#Plot the distribution:
-ftypes = ['STA to STA','STA to AP','AP to STA','AP to AP']
-pos = np.arange(len(ftypes))
-data.groupby('wlan.fc.ds')['frame.number'].nunique().plot(kind='bar')
-plt.title("Distribution of Traffic Direction")
-plt.xlabel("Traffic Direction")
-plt.ylabel("Number of Packets")
-plt.xticks(pos,ftypes,rotation=0)
-plt.show()
-
-
-# In[12]:
+# In[ ]:
 
 
 #What QoS does the data flows have?
-data['wlan.qos.priority'].value_counts()
+qos=list(data['wlan.qos.priority'].value_counts())
+print(qos)
+total_qos =sum(qos)
+for x in range(len(qos)):
+    qos[x] = (qos[x]/total_qos)*100
+print(qos)
 
 
-# In[13]:
+# In[ ]:
+
 
 
 #Plot the distribution:
-ftypes = ['Best Effort','Spare(Background)','Video (Controlled Load)','Video','Voice']
+ftypes = ['Best Effort','Spare \n (Background)','Video \n (Controlled Load)','Video','Voice']
 pos = np.arange(len(ftypes))
-data.groupby('wlan.qos.priority')['frame.number'].nunique().plot(kind='bar')
+rects3 = plt.bar(x=pos, height=qos, width=0.5, align='center', color=['#17BECF','#1F77B4','#FF7F0E','#2CA02C','#D62728'])
 plt.title("Distribution of QoS Traffic")
 plt.xlabel("QoS Class")
 plt.ylabel("Number of Packets")
-plt.xticks(pos,ftypes,rotation=0)
+plt.xticks(pos,ftypes,rotation=0, fontsize=8)
+
+autolabel(rects3)
 plt.show()
 
 
-# In[14]:
+# In[ ]:
 
 
 #Max data transferred in a packet(bytes)
 data['data.len'].max()
 
 
-# In[15]:
+# In[ ]:
 
 
 #Min data transferred in a packet(bytes)
 data['data.len'].min()
 
 
-# In[16]:
+# In[ ]:
 
 
 #Plot the distribution of data length (bytes):
@@ -149,7 +176,7 @@ data[['data.len']].plot(kind='hist',bins=bin1,rwidth=0.8)
 plt.show()
 
 
-# In[17]:
+# In[ ]:
 
 
 #Plot the distribution of data length (bytes):
@@ -158,10 +185,63 @@ data[['data.len']].plot(kind='hist',bins=bin1,rwidth=0.8)
 plt.show()
 
 
-# In[18]:
+# In[ ]:
 
 
 data['data.len'].value_counts()
+
+
+# In[ ]:
+
+
+#Packet Size over Time
+plt.plot(data['frame.time_relative'], data['data.len'])
+
+#bin1 = [i for i in range(0,1600,100)]
+#data[['data.len']].plot(kind='hist',bins=bin1,rwidth=0.8)
+plt.show()
+
+
+# In[ ]:
+
+
+#Packet Size over Time
+plt.scatter(data['wlan_radio.signal_dbm'], data['wlan_radio.data_rate'],marker='X',s=1**2)
+
+#bin1 = [i for i in range(0,1600,100)]
+#data[['data.len']].plot(kind='hist',bins=bin1,rwidth=0.8)
+plt.show()
+
+
+# In[ ]:
+
+
+data[['wlan_radio.signal_dbm']].plot(kind='bar')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
--- log.md
+++ log.md
@@ -4,13 +4,13 @@
 netflix 		| 	Application			|    10 min 	|	At night, in Dorm ~ 21:30-21:40
 torrent 		| 	Application (DL)    |	 5 min 		|	At morning, in Ewi Lobby ~ 11:08-~11:13  
 instagram 		| 	Application	 		|	 10 min    	|	At night, in Dorm ~ 20:15-20:25
-*Browsing 		| 	Application			|	 10 min 	|
+Browsing 		| 	Application			|	 10 min 	|   At noon, in Dorm ~ 14:00-14:10
 *drive 			| 	Application			|	 10 min 	|	At night, in Dorm ~
 Youtube 		| 	Application			|	 10 min  	|	At morning, in Dorm ~ 8:00-8:10
 WhatsApp Call 	| 	Application			|	 10 min 	|				
-*FaceTime		|	Application			|	 10 min 	|
+FaceTime		|	Application			|	 10 min 	|
 WhatsApp Video 	|	Application 		|	 10 min 	|
-*Stieltjesweg	|	Location			|	 10 min 	|	At night, in Dorm ~
+Stieltjesweg	|	Location			|	 10 min 	|	At noon, in Dorm ~ 14:10-14:20
 cafeteria_2 	|	Location			|	 10 min		|	At Noon, in Ewi Cafeteria ~ 15:15-15:25
 library     	|	Location			|	 60 min  	|   At Noon, in Library Floor 1 ~ 12:45-13:45 ON SATURDAY
 MDR				|	Location			|	 10 min 	|	At night, in Dorm ~
